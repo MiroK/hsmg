3b8d8d709f785f9d9dedfb2c9b39daf669d67c85
trygvba <trygve.baerland@gmail.com>
2017-10-09 14:10:46 +0200

    Added basic docstrings to get my bearing.

--- hsmg/hs_multigrid.py
+++ hsmg/hs_multigrid.py
@@ -2,10 +2,37 @@
 # action of numpy array
 
 def setup(A, M, R, bdry_dofs, macro_dofmap):
-    '''TODO'''
+    '''
+    Factory function for creating instances of MG methods for fractional Sobolev norms.
+    INPUT:
+        A: Stiffness matrices
+        M: Mass matrices
+        R: Restriction matrices
+        bdry_dofs: Boundary DOFs on each mesh level in hierarchy.
+        macro_dofmap: For each level tabulates DOFs for each subdomain, to be used in smoother.
+    OUTPUT:
+        res: Returns instance of MG-method that works on vectors, and can be called via __call__(b).
+    '''
     # Dummy for testing
-    class Foo(object):
-        def __call__(self, x):
-            return A.dot(x)
+    class FracLapMG(object):
+        '''Class for MG method''' 
+        def __init__(self, A, M, R, bdry_dofs, macro_dofmap):
+            '''Constructor'''
+            raise NotImplementedError
+        def multigrid_level(self,j,bj):
+            '''Multigrid method on general level.
+            INPUT:
+                j: integer; level in mesh hierarchy
+                bj: vector to apply MG method to.
+            RETURN:
+                res: Output vector from method.
+            '''
+            raise NotImplementedError
 
-    return Foo()
+        def coarse_solve(self, bj):
+            '''Solve exactly on coarsest level of mesh hierarchy.'''
+        def __call__(self, b):
+            '''Call method.'''
+            return self.multigrid_level(0,b)
+
+    return FracLapMG(A,M,R, bdry_dofs, macro_dofmap)
