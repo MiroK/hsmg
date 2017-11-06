014c8d098a8a8649a7a1f5b8ea595031b0f12cd5
mirok <miroslav.kuchta@gmail.com>
2017-11-06 11:08:41 +0100

    test macroelemnt on embedded meshes

--- demo/snorm_mg.py
+++ demo/snorm_mg.py
@@ -34,9 +34,9 @@ def main(hierarchy, s):
     xrand -= xrand.sum()/x.local_size()
     x.set_local(xrand)
 
-    b = x.copy()
+    b = Function(V).vector()
 
-    Ainv = ConjGrad(A, precond=B, tolerance=1e-13, maxiter=500, show=2)
+    Ainv = ConjGrad(A, precond=B, initial_guess=x, tolerance=1e-13, maxiter=500, show=2)
 
     # Compute solution
     x = Ainv * b
