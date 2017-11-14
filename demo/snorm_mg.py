75663a697a46f54ada5e7403c42e36799cd4730f
mirok <miroslav.kuchta@gmail.com>
2017-11-14 18:00:38 +0100

    bcs with snorm

--- demo/snorm_mg.py
+++ demo/snorm_mg.py
@@ -10,20 +10,24 @@ from dolfin import *
 import numpy as np
 
 
-def main(hierarchy, s):
+def main(hierarchy, s, bcs=False):
     '''
     Solve Ax = b where A is the eigenvalue representation of (-Delta + I)^s
     '''
     mesh = hierarchy[0]
     V = FunctionSpace(mesh, 'CG', 1)
-    
-    A = H1_L2_InterpolationNorm(V).get_s_norm(s=s, as_type=PETScMatrix)
+
+    if bcs:
+        bdry = DomainBoundary()
+        bcs = DirichletBC(V, Constant(0), bdry)
+    else:
+        bdry = None
+        bcs = []
+    A = H1_L2_InterpolationNorm(V, bcs=bcs).get_s_norm(s=s, as_type=PETScMatrix)
     
     mg_params = {'macro_size': 1,
                  'nlevels': len(hierarchy),
                  'eta': 1.0}
-    # FIXME, bdry = None does not work at the moment
-    bdry = None
     B = HsNormMG(V, bdry, s, mg_params, mesh_hierarchy=hierarchy)  
 
     x = Function(V).vector()
@@ -33,11 +37,15 @@ def main(hierarchy, s):
     x.set_local(xrand)
 
     # Rhs
-    v = TestFunction(V)
+    u, v = TrialFunction(V), TestFunction(V)
     f = Expression('sin(k*pi*x[0])', k=1, degree=4)
-    b = assemble(inner(f, v)*dx)
     # b = Function(V).vector()
-
+    if bdry is not None:
+        _, b = assemble_system(inner(u, v)*dx, inner(f, v)*dx, bcs)
+        bcs.apply(x)
+    else:
+        b = assemble(inner(f, v)*dx)
+        
     Ainv = ConjGrad(A, precond=B, initial_guess=x, tolerance=1e-13, maxiter=500, show=2)
 
     # Compute solution
@@ -61,6 +69,8 @@ if __name__ == '__main__':
     import argparse
     import re
 
+    # python snorm_mg.py "-0.6 : 0.01 : -0.4" -D 1 -n 7 -log ./results/h1.txt
+    
     D_options = ['1', '2',  # Xd in Xd
                  '12', '13', '23',  # Xd in Yd no intersect
                  '-12', '-13', '-23',  # Xd in Yd isect
@@ -73,11 +83,14 @@ if __name__ == '__main__':
                         default='1', choices=['all'] + D_options)  # Xd in Yd loop
     parser.add_argument('-n', type=int, help='Number of refinements of initial mesh',
                         default=4)
+    parser.add_argument('-bcs', type=int, help='Apply boundary conditions',
+                        default=0)
     parser.add_argument('-log', type=str, help='Path to file for storing results',
                          default='')
     args = parser.parse_args()
 
     # Fractionality series
+    print args.s
     try:
         s_values = [float(args.s)]
     except ValueError:
@@ -97,7 +110,7 @@ if __name__ == '__main__':
             for n in [2**i for i in range(5, 5+args.n)]:
                 hierarchy = compute_hierarchy(D, n, nlevels=4)
 
-                size, niters, cond = main(hierarchy, s=s)
+                size, niters, cond = main(hierarchy, s=s, bcs=bool(args.bcs))
 
                 msg = 'Problem size %d, current iters %d, cond %g, previous %r'
                 print '\033[1;37;31m%s\033[0m' % (msg % (size, niters, cond, history[::-1]))
