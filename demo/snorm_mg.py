76f12711a15964af515aed8a035b3d8bfd29ad54
trygvba <trygve.baerland@gmail.com>
2018-07-10 11:10:20 +0200

    Indentation inconsistency.

--- demo/snorm_mg.py
+++ demo/snorm_mg.py
@@ -35,7 +35,6 @@ def main(hierarchy, s):
 
     # Zero
     b = Function(V).vector()
-
     Ainv = ConjGrad(A, precond=B, initial_guess=x, tolerance=1e-13, maxiter=500, show=2)
 
     # Compute solution
