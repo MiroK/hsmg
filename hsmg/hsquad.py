04e417fa1ab387e63e25c766c09a7a5e95d1f43d
mirok <miroslav.kuchta@gmail.com>
2018-08-16 14:30:41 +0200

    rewrite hsquad/doc/tests

--- hsmg/hsquad.py
+++ hsmg/hsquad.py
@@ -114,243 +114,91 @@ class BPOperator(block_base):
 
         return x, nsolves, iter_count
 
-    # API that the child has to implement ---------------------------
-    def compute_k(self, fractionality, space_dim, mesh_size):
-        raise NotImplementedError()
+    def shifted_problem(self, shift):
+        raise NotImplementedError
 
-    def shifted_operator(self, shift):
-        raise NotImplementedError()
-
-    def solve_shifted_problem(self, operator, x, vector):
-        raise NotImplementedError()
-
-    # Implementation of cbc.block API --------------------------------
-
-    def matvec(self, b):
-        return self(b)
-
-    @vec_pool
-    def create_vec(self, dim=1):
-        return Function(self.V).vector()
-
-######################################################################
-# Specializations
-######################################################################
-
-def Zero(V):
-    '''Zero element of V'''
-    return Constant(zero(V.ufl_element().value_shape()))
-
-class BP_HsNorm_Base(BP_Operator_Base):
-    '''Hs norms using direct solver'''
-    @classmethod
-    def prepare_bcs(cls, V, bcs):
-        '''whatever -> DirichletBC'''
-        try:
-            return [prepare_bcs(cls, V, b) for b in bcs]
-        except TypeError:
-            pass
-        
-        if bcs is None or isinstance(bcs, DirichletBC):
-            return bcs
-
-        return DirichletBC(V, Zero(V), bcs)
     
-    def __init__(self, V, bcs, s, parameters):
-        '''Children need to give A, I, ... '''
-        self.bcs = BP_HsNorm_Base.prepare_bcs(V, bcs)
-        # A dummy right hand side for symmetric assembly
-        self.L = inner(Zero(V), TestFunction(V))*dx
-        # Get stuff for specialization
-        BP_Operator_Base.__init__(self, s, V)
+class FHelmholtz(BPOperator):
+    '''BPOperator with L = -Delta + I; fractional Helmholtz'''
+    def __init__(self, V, s, bcs, solve_shifted_problem=None, compute_k=0.5):
         
-        # Cook up API functions
-        parameters = parameters.copy()
-        # quadrature
-        f = parameters.pop('k')
-        if isinstance(f, (int, float)):
-            compute_k = (lambda s, N, h: f)
-        else:
-            assert isinstance(f, FunctionType)
-            assert f.func_code.co_argcount == 3
-            compute_k = f 
-        self.compute_k = compute_k
+        BPOperator.__init__(self, V, s, solve_shifted_problem, compute_k)
 
-        # Solver, just for fun trying to avoid assignments
-        # Direct + matrix
-        solver = parameters.pop('solver')
-        if solver == 'lu':
-            # LU
-            solve_shifted_problem = lambda A, x, b: (solve(A, x, b), x)
-            # System
-            shifted_operator = \
-                lambda self, shift: assemble_system(self.A + Constant(shift)*self.I,
-                                                    self.L,
-                                                    self.bcs)[0]
-        # Cholesky from cbc.block
-        elif solver == 'cholesky':
-            # Solver
-            solve_shifted_problem = lambda A, x, b: (1, (lambda: Cholesky(A))()*b)
-            # System
-            shifted_operator = \
-                               lambda self, shift: assemble_system(self.A + Constant(shift)*self.I,
-                                                                   self.L,
-                                                                   self.bcs)[0]
-        # Iterative
-        else:
-            assert solver == 'iterative'
-            self.S = None
-            self.solver = None
-            
-            # Action of the shifted operator
-            def shifted_operator(self, shift):
-                # Update and return
-                if self.S is not None:
-                    self.S.shift = shift
-                    return self.S
-
-                # Setup
-                A, _ = assemble_system(self.A, self.L, self.bcs)
-                I, _ = assemble_system(self.I, self.L, self.bcs)
-
-                class ShiftedOperator(LinearOperator):
-                    def __init__(self, shift):
-                        self.shift = shift
-                        LinearOperator.__init__(self, Function(V).vector(), Function(V).vector())
-                        
-                    def mult(self, x, y):
-                        z = I*x
-                        A.mult(x, y)
-                        y.axpy(self.shift, z)
-                # One instance of it
-                S = ShiftedOperator(shift)
-                self.S = S
-
-                # Setup the solver here as well
-                # Take A as a preconditioner
-                Bmat, _ = assemble_system(self.A + self.I, self.L, self.bcs)
-                B = PETScPreconditioner('hypre_amg')
-                # Should be SPD
-                solver = PETScKrylovSolver('cg', B)
-                solver.set_operators(S, Bmat)
-                solver.parameters.update(parameters.pop('krylov_parameters'))
-                
-                self.solver = solver
-
-                # Done
-                return S
-
-            def solve_shifted_problem(A, x, b):
-                assert self.solver is not None
-                niters = self.solver.solve(x, b)
-                return niters, x
-            
-        # Assign bodies
-        self.solve_shifted_problem_ = solve_shifted_problem
-        self.shifted_operator_ = shifted_operator
-        
-    # BP_Operator_Base interface
-    
-    def compute_k(self, s, N, h):
-        return self.compute_k(s, N, h)
-
-    def shifted_operator(self, shift):
-        '''Return a matrix'''
-        return self.shifted_operator_(self, shift) 
-
-    def solve_shifted_problem(self, A, x, b):
-        '''Direct by default'''
-        return self.solve_shifted_problem_(A, x, b)
-
-######################################################################
-# Concrete
-######################################################################
-
-class BP_H10Norm(BP_HsNorm_Base):
-    '''Dirichlet fractional Laplacian'''
-    def __init__(self, V, bcs, s, params):
-        assert bcs is not None
-
+        # For operator build
         u, v = TrialFunction(V), TestFunction(V)
-        h = CellSize(V.mesh())
-
-        if V.ufl_element().family() == 'Discontinuous Lagrange':
-            assert V.ufl_element().value_shape() == ()
-            assert V.ufl_element().degree() == 0
-
-            h_avg = avg(h)            
-            a = h_avg**(-1)*dot(jump(v), jump(u))*dS + h**(-1)*dot(u, v)*ds
-        else:
-            a = inner(grad(u), grad(v))*dx
-
-        BP_HsNorm_Base.__init__(self, V, bcs, s, params)
-        self.A = a
-        self.I = inner(u, v)*dx
-
-        
-class BP_H1Norm(BP_HsNorm_Base):
-    '''Neumannn fractional Helmholtz'''
-    def __init__(self, V, s, params):
-
-        u, v = TrialFunction(V), TestFunction(V)
-        h = CellSize(V.mesh())
+        h = CellDiameter(V.mesh())
 
         if V.ufl_element().family() == 'Discontinuous Lagrange':
             assert V.ufl_element().value_shape() == ()
             assert V.ufl_element().degree() == 0
 
             h_avg = avg(h)
-            a = h_avg**(-1)*dot(jump(v), jump(u))*dS + inner(u, v)*dx
+            a = h_avg**(-1)*dot(jump(v), jump(u))*dS + inner(u, v)*ds + inner(u, v)*dx
         else:
-            a = inner(grad(u), grad(v))*dx + inner(u, v)*dx
+            a = inner(grad(u), grad(v))*dx + inner(u, v)*dx            
 
-        bcs = None
-        BP_HsNorm_Base.__init__(self, V, bcs, s, params)
-        self.A = a
+        self.L = a
         self.I = inner(u, v)*dx
+        self.f = inner(Constant(np.zeros(V.ufl_element().value_shape())), v)*dx
 
+        self.bcs = bcs
+
+    def shifted_operator(self, shift):
+        return assemble_system(self.L + Constant(shift)*self.I, self.f, self.bcs)[0]
+
+        
+class FLaplace(BPOperator):
+    '''BPOperator with L = -Delta, fractional Laplacian'''
+    def __init__(self, V, s, bcs, solve_shifted_problem=None, compute_k=0.5):
+        assert bcs is not None
+        BPOperator.__init__(self, V, s, solve_shifted_problem, compute_k)
+
+        # For operator build
+        u, v = TrialFunction(V), TestFunction(V)
+        h = CellDiameter(V.mesh())
+
+        if V.ufl_element().family() == 'Discontinuous Lagrange':
+            assert V.ufl_element().value_shape() == ()
+            assert V.ufl_element().degree() == 0
+
+            h_avg = avg(h)
+            a = h_avg**(-1)*dot(jump(v), jump(u))*dS + inner(u, v)*ds 
+        else:
+            a = inner(grad(u), grad(v))*dx
+
+        self.L = a
+        self.I = inner(u, v)*dx
+        self.f = inner(Constant(np.zeros(V.ufl_element().value_shape())), v)*dx
+        self.bcs = bcs
+
+    def shifted_operator(self, shift):
+        return assemble_system(self.L + Constant(shift)*self.I, self.f, self.bcs)[0]
+
+    
 # -------------------------------------------------------------------
+
 
 # NOTE: this is for debugging
 if __name__ == '__main__':
     from dolfin import UnitIntervalMesh, FunctionSpace, Expression, assemble
-    from dolfin import plot, interactive, Function, interpolate, errornorm, ln
+    from dolfin import Function, interpolate, errornorm, ln
     from dolfin import UnitSquareMesh
     
-    s = -0.9
-    k = 1.
+    s = 0.5
+    k = 2.
     
-    params = {'k': 0.25,
-              'solver': 'cholesky',
-              'krylov_parameters': {'relative_tolerance': 1E-8,
-                                    'absolute_tolerance': 1E-8,
-                                    'convergence_norm_type': 'true',
-                                    'monitor_convergence': False}}
     if True:
         f = Expression('sin(k*pi*x[0])', k=k, degree=4)
         u_exact = Expression('sin(k*pi*x[0])/pow(pow(k*pi, 2), s)', s=s, k=k, degree=4)
 
-        # f = Expression('sin(k*pi*x[0])*sin(l*pi*x[1])', k=k, l=2*k, degree=4)
-        # u_exact = Expression('sin(k*pi*x[0])*sin(l*pi*x[1])/pow(pow(k*pi, 2) + pow(l*pi, 2), s)',
-        #                     s=s, k=k, l=2*k, degree=4)
-
-        
-        # f = Expression('sin(k*pi*x[0])', k=k, degree=4)
-        # u_exact = Expression('sin(k*pi*x[0])/pow(pow(k*pi, 2), s)', s=s, k=k, degree=4)
-        
-        
         get_bcs = lambda V: DirichletBC(V, Constant(0), 'on_boundary')
-        get_B = lambda V, bcs, s: BP_H10Norm(V, bcs, s, params=params)
+        get_B = lambda V, bcs, s: FLaplace(V, s, bcs)
     else:
         f = Expression('cos(k*pi*x[0])', k=k, degree=4)
         u_exact = Expression('cos(k*pi*x[0])/pow(pow(k*pi, 2) + 1, s)', s=s, k=k, degree=4)
 
-        f = Expression('cos(k*pi*x[0])*cos(k*pi*x[1])', k=k, degree=4)
-        u_exact = Expression('cos(k*pi*x[0])*cos(k*pi*x[1])/pow(2*pow(k*pi, 2) + 1, s)', s=s, k=k, degree=4)
-
         get_bcs = lambda V: None
-        get_B = lambda V, bcs, s: BP_H1Norm(V, s, params=params)
-    
+        get_B = lambda V, bcs, s: FHelmholtz(V, s, bcs)    
     e0 = None
     h0 = None
     for n in [2**i for i in range(2, 7)]: #[2**i for i in range(5, 13)]:
@@ -359,7 +207,6 @@ if __name__ == '__main__':
         V = FunctionSpace(mesh, 'CG', 1)
         # V = FunctionSpace(mesh, 'DG', 0)
         bcs = get_bcs(V)
-
         B = get_B(V, bcs, s)
 
         u, v = TrialFunction(V), TestFunction(V)
@@ -386,16 +233,3 @@ if __name__ == '__main__':
 
     u_exact = interpolate(u_exact, V)
     u_exact.vector().axpy(-1, df.vector())
-
-    plot(u_exact)
-    interactive()
-
-    # xx = V.tabulate_dof_coordinates()
-    
-    # import matplotlib.pyplot as plt
-
-    # plt.figure()
-    # plt.plot(xx, u_exact.vector().array(), label='exact')
-    # plt.plot(xx, df.vector().array(), label='numeric')
-    # plt.legend(loc='best')
-    # plt.show()
--- tests/test_hsquad.py
+++ tests/test_hsquad.py
@@ -1,152 +1,71 @@
-from hsmg.hsquad import BP_H1Norm, BP_H10Norm
 from dolfin import *
-import pytest
+from hsmg.hsquad import FLaplace, FHelmholtz
+from hsmg.utils import from_np_array
+import numpy as np
+
+import unittest
 
 
-# FIXME: with DG0 works okay for s > 0, s < 0 gets works the smaller
-# s is and it seems that all the error is on the boundary
-@pytest.mark.parametrize('solver', ['cholesky', 'iterative'])
-@pytest.mark.parametrize('element', [FiniteElement('Lagrange', interval, 1)])
-@pytest.mark.parametrize('s_value', [-0.9, -0.5, -0.1, 0.1, 0.5, 0.9])
-def test_poisson_1d(solver, s_value, element):
-    params = {'k': 0.25, # This should be small enough to get order for all
-              'krylov_parameters': {'relative_tolerance': 1E-8,
-                                    'absolute_tolerance': 1E-8,
-                                    'convergence_norm_type': 'true',
-                                    'monitor_convergence': False}}
-    params['solver'] = solver
+class TestHsNorm(unittest.TestCase):
+    def test_def(self):
+        '''Approximation of truth'''
+        k = 2
+        f = Expression('cos(k*pi*x[0])', k=k, degree=4)
 
-    k = 1.
-    f = Expression('sin(k*pi*x[0])', k=k, degree=4)
-    u_exact = Expression('sin(k*pi*x[0])/pow(pow(k*pi, 2), s)', s=s_value, k=k, degree=4)
+        # Solve (-Delta + I)^s u_exact = f
+        for s in (0.25, 0.5, 0.75, -0.25):
+            hs, errors = [], []
+            u_exact = Expression('cos(k*pi*x[0])/pow(pow(k*pi, 2) + 1, s)', s=s, k=k, degree=4)
+            
+            for n in [8, 16, 32, 64, 128]:
+                mesh = UnitIntervalMesh(n)
 
-    get_bcs = lambda V: DirichletBC(V, Constant(0), 'on_boundary')
-    get_B = lambda V, bcs, s: BP_H10Norm(V, bcs, s, params=params)
+                V = FunctionSpace(mesh, 'CG', 1)
 
-    e0 = None
-    h0 = None
-    for n in [2**i for i in range(2, 5)]:
-        mesh = UnitIntervalMesh(n)
-        
-        V = FunctionSpace(mesh, element)
-        bcs = get_bcs(V)
+                b = assemble(inner(TestFunction(V), f)*dx)
+                # Numeric
+                H = FHelmholtz(V, s, bcs=None, compute_k=0.5)
+                # NOTE: so the map is nodal to dual
+                uh = Function(V, H*b)
 
-        B = get_B(V, bcs, s_value)
+                error = errornorm(u_exact, uh, 'L2')
+                hs.append(mesh.hmin())
+                errors.append(error)
+            # Decrease
+            self.assertTrue(all(np.diff(errors) < 0))
+            # Order
+            deg = np.polyfit(np.log(hs), np.log(errors), 1)[0]
+            self.assertTrue(deg > 1)
 
-        v = TestFunction(V)
-        b = assemble(inner(f, v)*dx)
-        if bcs is not None: bcs.apply(b)
+            
+class TestHs0Norm(unittest.TestCase):
+    def test_def(self):
+        '''Approximation of truth'''
+        k = 2
+        f = Expression('sin(k*pi*x[0])', k=k, degree=4)
 
-        x = B*b
+        # Solve (-Delta + I)^s u_exact = f
+        for s in (0.25, 0.5, 0.75, -0.25):
+            hs, errors = [], []
+            u_exact = Expression('sin(k*pi*x[0])/pow(pow(k*pi, 2) + 1, s)', s=s, k=k, degree=4)
+            
+            for n in [8, 16, 32, 64, 128]:
+                mesh = UnitIntervalMesh(n)
 
-        df = Function(V, x)
-        h = mesh.hmin()
-        e = errornorm(u_exact, df, 'L2', degree_rise=3)
-        if e0 is None:
-            rate = 0.
-        else:
-            rate = ln(e/e0)/ln(h/h0)
-        print rate
-        e0 = e
-        h0 = float(h)
-    print rate
-    assert any(((element.degree() == 1 and rate > 1.9),
-                (element.degree() == 0 and rate > 0.9)))
+                V = FunctionSpace(mesh, 'CG', 1)
 
+                b = assemble(inner(TestFunction(V), f)*dx)
+                # Numeric
+                bcs = DirichletBC(V, Constant(0), 'on_boundary')
+                H = FLaplace(V, s, bcs, compute_k=0.5)
+                # NOTE: so the map is nodal to dual
+                uh = Function(V, H*b)
 
-@pytest.mark.parametrize('solver', ['cholesky', 'iterative'])
-@pytest.mark.parametrize('element', [FiniteElement('Lagrange', interval, 1),
-                                     FiniteElement('Discontinuous Lagrange', interval, 0)])
-@pytest.mark.parametrize('s_value', [-0.9, -0.5, -0.1, 0.1, 0.5, 0.9])
-def test_helmholtz_1d(solver, s_value, element):
-    params = {'k': 0.25, # This should be small enough to get order for all
-              'krylov_parameters': {'relative_tolerance': 1E-8,
-                                    'absolute_tolerance': 1E-8,
-                                    'convergence_norm_type': 'true',
-                                    'monitor_convergence': False}}
-    params['solver'] = solver
-
-    k = 1.
-    f = Expression('cos(k*pi*x[0])', k=k, degree=4)
-    u_exact = Expression('cos(k*pi*x[0])/pow(pow(k*pi, 2) + 1, s)',
-                         s=s_value, k=k, degree=4)
-
-    get_B = lambda V, bcs, s: BP_H1Norm(V, s, params=params)
-
-    e0 = None
-    h0 = None
-    for n in [2**i for i in range(2, 5)]:
-        mesh = UnitIntervalMesh(n)
-        
-        V = FunctionSpace(mesh, element)
-        bcs = None
-
-        B = get_B(V, bcs, s_value)
-
-        v = TestFunction(V)
-        b = assemble(inner(f, v)*dx)
-
-        x = B*b
-
-        df = Function(V, x)
-        h = mesh.hmin()
-        e = errornorm(u_exact, df, 'L2', degree_rise=3)
-        if e0 is None:
-            rate = 0
-        else:
-            rate = ln(e/e0)/ln(h/h0)
-        print rate
-        e0 = e
-        h0 = float(h)
-    print rate
-    assert any(((element.degree() == 1 and rate > 1.9),
-                (element.degree() == 0 and rate > 0.9)))
-
-
-# FIXME: DG0 does not work for neither positive nor negarive s
-@pytest.mark.parametrize('solver', ['cholesky', 'iterative'])
-@pytest.mark.parametrize('element', [FiniteElement('Lagrange', triangle, 1)])
-@pytest.mark.parametrize('s_value', [-0.9, -0.5, -0.1, 0.1, 0.5, 0.9])
-def test_helmholtz_2d(solver, s_value, element):
-    params = {'k': 0.25, # This should be small enough to get order for all
-              'krylov_parameters': {'relative_tolerance': 1E-8,
-                                    'absolute_tolerance': 1E-8,
-                                    'convergence_norm_type': 'true',
-                                    'monitor_convergence': False}}
-    params['solver'] = solver
-
-    k = 1.
-    f = Expression('cos(k*pi*x[0])*cos(k*pi*x[1])', k=k, degree=4)
-    u_exact = Expression('cos(k*pi*x[0])*cos(k*pi*x[1])/pow(2*pow(k*pi, 2) + 1, s)',
-                         s=s_value, k=k, degree=4)
-
-
-    get_B = lambda V, bcs, s: BP_H1Norm(V, s, params=params)
-
-    e0 = None
-    h0 = None
-    for n in [2**i for i in range(2, 5)]:
-        mesh = UnitSquareMesh(n, n)
-        
-        V = FunctionSpace(mesh, element)
-        bcs = None
-
-        B = get_B(V, bcs, s_value)
-
-        v = TestFunction(V)
-        b = assemble(inner(f, v)*dx)
-
-        x = B*b
-
-        df = Function(V, x)
-        h = mesh.hmin()
-        e = errornorm(u_exact, df, 'L2', degree_rise=3)
-        if e0 is None:
-            rate = 0
-        else:
-            rate = ln(e/e0)/ln(h/h0)
-        print rate
-        e0 = e
-        h0 = float(h)
-    print rate
-    assert element.degree() == 1 and rate > 1.0
+                error = errornorm(u_exact, uh, 'L2')
+                hs.append(mesh.hmin())
+                errors.append(error)
+            # Decrease
+            self.assertTrue(all(np.diff(errors) < 0))
+            # Order
+            deg = np.polyfit(np.log(hs), np.log(errors), 1)[0]
+            self.assertTrue(deg > 1)
