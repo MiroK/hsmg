6164ef40193e801582b3fc6b59ffdd2b2de44831
Kent-Andre Mardal <kent-and@simula.no>
2021-03-16 16:36:36 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(name = "hsmg",
+      version = "0.0.1",
+      description = "Multigrid for fractional operators",
+      author = "Miroslav Kuchta",
+      author_email = "miroslav.kuchta@gmail.com",
+      url = "https://github.com/MiroK/hsmg", 
+      packages = ["hsmg"]
+)
+
